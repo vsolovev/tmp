7609358310df3da7648b0d3fb90dd1434d5edbc4
Vladimir Solovev <hydraca39@gmail.com>
2025-07-22 11:52:04 +0300

    add generated

---
+++ ErrTest/GeneratedFiles/ErrorManager/ErrorCodeSourceGenerator/ErrorManager.g.cs
@@ -0,0 +1,17 @@
+﻿using ErrTest;
+public static class ErrorManager {
+    public static class MyCompany {
+        public static class MyService {
+            public static class CartElement {
+                public static class Request {
+                    public static readonly ErrorCode Inaccessible = new ErrorCode("MyCompany.MyService.CartElement.Request.Inaccessible", "The requested card element is not accessible", 403);
+                }
+            }
+            public static class CartContent {
+                public static class Request {
+                    public static readonly ErrorCode InternalError = new ErrorCode("MyCompany.MyService.CartContent.Request.InternalError", "Error requesting cart content", 500);
+                }
+            }
+        }
+    }
+}
